e14b5e9f9beb4090185efa4a09f88b19eece8011
Kamil Fron <kamilfron91@gmail.com>
2024-12-18 23:48:19 +0100

    Dodanie wsparcia dla CPT i widgetow

--- functions.php
+++ functions.php
@@ -16,6 +16,39 @@ function zwiazek_chemikow_setup()
         'footer' => __('Menu w stopce', 'aurora')
     ));
 }
+
+// Rejestracja Custom Post Type dla dokumentów
+function zwiazek_chemikow_register_post_types()
+{
+    register_post_type('document', array(
+        'labels' => array(
+            'name' => 'Dokumenty',
+            'singular_name' => 'Dokument'
+        ),
+        'public' => true,
+        'has_archive' => true,
+        'menu_icon' => 'dashicons-media-document',
+        'supports' => array('title', 'editor', 'thumbnail')
+    ));
+}
+add_action('init', 'zwiazek_chemikow_register_post_types');
+
+// Rejestracja sidebars
+function zwiazek_chemikow_widgets_init()
+{
+    register_sidebar(array(
+        'name'          => 'Sidebar',
+        'id'            => 'sidebar-1',
+        'description'   => 'Główny sidebar',
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<h2 class="widget-title">',
+        'after_title'   => '</h2>',
+    ));
+}
+add_action('widgets_init', 'zwiazek_chemikow_widgets_init');
+
+
 add_action('after_setup_theme', 'zwiazek_chemikow_setup');
 
 // Dodanie styli i skryptów
